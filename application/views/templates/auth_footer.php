8859335efececa8b8292a9366f18074dae27b8ce
pill0wl <fakhririfi@gmail.com>
2019-03-25 15:46:43 +0700

    WEB ni boi

---
+++ application/views/templates/auth_footer.php
@@ -0,0 +1,13 @@
+<!-- Bootstrap core JavaScript-->
+<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url(); ?>assets/vendor/sbadmin2/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html> 
